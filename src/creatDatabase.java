d0a46b549ed49b2d4745717b5a55329a7617865b
tzz <1789803837@qq.com>
2018-04-27 14:04:09 +0800

    数据库连接

---
+++ src/creatDatabase.java
@@ -0,0 +1,5 @@
+public class creatDatabase {
+    public creatDatabase() {
+
+    }
+}
--- src/database.java
+++ src/database.java
@@ -1,15 +1,41 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class database {
     private String url;
-    private String usr;
-    private String pwd;
+    private String usr = "root";
+    private String pwd = "root";
     private Connection connection;
 
 
-    public database() {
-
+    public database(String url, String usr, String pwd) {
+        this.url = url;
+        this.usr = usr;
+        this.pwd = pwd;
     }
 
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(url, usr, pwd);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 
 }
