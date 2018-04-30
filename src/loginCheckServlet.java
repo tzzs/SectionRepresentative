dada9d291e7d89a2e541d191d753607a7c61eebf
tzz <1789803837@qq.com>
2018-04-30 20:47:54 +0800

    add homeworkDao

--- src/loginCheckServlet.java
+++ src/loginCheckServlet.java
@@ -11,38 +11,33 @@ public class loginCheckServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         HttpSession session = request.getSession();
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("remember") && cookie.getValue().equals("true")) {
-                //无需登录
-            }
-        }
+//        for (Cookie cookie : cookies) {
+//            if (cookie.getName().equals("remember") && cookie.getValue().equals("true")) {
+//                //无需登录
+//            }
+//        }
 
         String account = request.getParameter("account");
         String password = request.getParameter("password");
         String[] remember = request.getParameterValues("remember");
 
-
-
-//        String pwd = ;
         userDao ud = new userDao();
-//        List<User> userList = ud.select(account);
         User user = ud.select(account);
 
 
-
         //获取服务器账号密码 对比
-        if (account.equals("") && password.equals("")) {
-            if (remember[0].equals("remember-me")) {
+        if (password.equals(user.getPassword())) {
+            if (remember[0].equals("remember")) {
                 Cookie newCookie = new Cookie("remember", "true");
             }
-
+            response.sendRedirect("html/main.html");
         } else {
-            request.getRequestDispatcher("html/login.html");
+            request.getRequestDispatcher("html/index.html");
             session.setAttribute("loginInfo", "账号或密码输入错误，请重新登录");
         }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request, response);
     }
 }
