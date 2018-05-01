a3f4efdf024cd6a33d9f52adcfb44e17e52fe1c3
tzz <1789803837@qq.com>
2018-05-01 13:28:05 +0800

    添加注销

---
+++ src/logoutServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "logoutServlet")
+public class logoutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("account")) {
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+        response.sendRedirect("html/index.html");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
