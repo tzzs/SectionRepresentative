a7ac26d8a9174a8f65c13ca145c4c5f747e1e272
tzz <1789803837@qq.com>
2018-05-11 15:51:06 +0800

    单个文件下载 附属文件下载

--- src/hfileDownloadServlet.java
+++ src/hfileDownloadServlet.java
@@ -6,11 +6,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 
-@WebServlet(name = "oneFileDownServlet", urlPatterns = "/fileDownloadServlet")
-public class oneFileDownServlet extends HttpServlet {
+@WebServlet(name = "hfileDownloadServlet", urlPatterns = "/hfileDownloadServlet")
+public class hfileDownloadServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String hno = request.getParameter("hno");
         System.out.println(hno);
+        String hfile = request.getParameter("hfile");
+        System.out.println(hfile);
 
         //使用cookie判断是否登录
         Cookie[] cookies = request.getCookies();
@@ -19,12 +21,14 @@ public class oneFileDownServlet extends HttpServlet {
                 String account = cookie.getValue();
 
                 //获取文件夹
-                String filename = null;
-                String dir = request.getServletContext().getRealPath("/WEB-INF/FILE");
-                File file = new File(dir, filename);
+//                String filename = null;
+                String dir = request.getServletContext().getRealPath("/WEB-INF/FILE/" + hno);
+                System.out.println("dir:" + dir);
+                File file = new File(dir, hfile);
+                System.out.println(file.getPath());
                 if (file.exists()) {
                     response.setContentType("application/pdf");
-                    response.setHeader("Content-Disposition", "attachment;filename=secret.pdf");
+                    response.setHeader("Content-Disposition", "attachment;filename=" + hfile);
                     byte[] buffer = new byte[1024];
                     FileInputStream fis = null;
                     BufferedInputStream bis = null;
@@ -56,6 +60,6 @@ public class oneFileDownServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request, response);
     }
 }
--- web/JS/searchWork.js
+++ web/JS/searchWork.js
@@ -1,21 +1,21 @@
 $(function () {
-	 $('#input_search').click(function () {
-        $.ajax({  
-            type: "POST",  
-            dataType: "json",  
-            url: "../searchWorkServlet", 
-            contentType : 'application/json;charset=utf-8',
+    $('#input_search').click(function () {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "../searchWorkServlet",
+            contentType: 'application/json;charset=utf-8',
             cache: false,
-            data:{  
-                "hno":"34" 
-            },  
-            success: function (json) { 
-            	$("#form-con1").val(json[0].hno);
-            },  
-            error: function () { 
-            	alert("查询失败!");
-            }  
-        }); 
-	 });
-	  console.log();
+            data: {
+                "hno": "34"
+            },
+            success: function (json) {
+                $("#form-con1").val(json[0].hno);
+            },
+            error: function () {
+                alert("查询失败!");
+            }
+        });
+    });
+    console.log();
 });  
