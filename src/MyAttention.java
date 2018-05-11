4fd7bda015e63c5d4a2550c25e11f594ff522870
tzz <1789803837@qq.com>
2018-05-11 20:01:07 +0800

    添加关注显示

--- src/MyAttention.java
+++ src/MyAttention.java
@@ -11,52 +11,45 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.List;
 
 /**
  * Servlet implementation class MyAttention
  */
 @WebServlet(name = "/MyAttention", urlPatterns = "/myAttention")
 public class MyAttention extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { ;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ;
         response.setContentType("text/html;charset=utf-8");
         request.setCharacterEncoding("utf-8");
         PrintWriter out = response.getWriter();
-        ArrayList<User> listInfor = new ArrayList<>();
-        ArrayList<String> list = new ArrayList<>();
+
         String account = "";
         String attention = "";
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals("account")) {
-                    System.out.println("amgrpk");
                     account = cookie.getValue();
-                    User user = new User();
-                    user.setAccount(account);
-                    System.out.println("this is " + user.getName());
+                    userDao ud = new userDao();
+                    User user = ud.select(account);
                     attention = user.getAttention();
-                    if (attention != null) {
-                        StringTokenizer stringTokenizer1 = new StringTokenizer(attention, ";");
-                        while (stringTokenizer1.hasMoreTokens()) {
-                            String string = "";
-                            string = stringTokenizer1.nextToken();
-                            list.add(string);
+//                    System.out.println("attention: " + attention);
+                    if (attention != null && !attention.equals("")) {
+                        String[] attentions = attention.split(";");
+//                        System.out.println(attentions[0]+attentions[1]+attentions[2]);
+                        List<User> userList = new ArrayList<>();
+                        for (String a : attentions) {
+                            userList.add(ud.select(a));
                         }
-                        for (int i = 0; i < list.size(); i++) {
-                            userDao userDao = new userDao();
-                            listInfor.add(userDao.select(list.get(i)));
-                        }
-                        JSONArray jsonArray = JSONArray.fromObject(listInfor);
-                        System.out.println("the is " + jsonArray);
+                        JSONArray jsonArray = JSONArray.fromObject(userList);
                         out.print(jsonArray);
-                        out.close();
-                        break;
                     }
                 }
             }
         } else {
-            System.out.println("a'mgep");
+            System.out.println("to login");
+            response.sendRedirect("html/index.html");
         }
     }
 
